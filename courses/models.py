3d672abebf1643ccf096fee07f529373ec16894a
tailyn-t <tiptd-26@rhodes.edu>
2025-02-11 16:05:15 -0600

    Initialization of Django project and database

---
+++ courses/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class CourseInfo(models.Model):
+    semester = models.CharField(max_length=255)
+    year = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    course_number = models.IntegerField()
+    course_title = models.CharField(max_length=255)
+    instructor = models.CharField(max_length=255)
+    max_enrollment = models.IntegerField()
+    students_enrolled = models.IntegerField()
+    primary_requests = models.IntegerField()
+    major_requests = models.IntegerField()
+    senior_requests = models.IntegerField()
+    seniors_enrolled = models.IntegerField()
+    junior_requests = models.IntegerField()
+    juniors_enrolled = models.IntegerField()
+    sophomore_requests = models.IntegerField()
+    sophomores_enrolled = models.IntegerField()
+    first_year_requests = models.IntegerField()
+    first_years_enrolled = models.IntegerField()
