9e31272d07060be5de456514f222af88049c2cdc
edwah-25 <122332923+edwah-25@users.noreply.github.com>
2025-02-17 23:10:08 -0600

    "Added Course waitlist size and does it typically have a waitlist"

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.db.models import Count, Avg
+from django.shortcuts import render
+from django.db.models import Sum, QuerySet, F, Avg, Value
 from courses.models import CourseInfo, CourseCatalog
+from django.db.models.functions import Greatest
 
 
 def home(request):
@@ -8,10 +9,42 @@ def home(request):
     return render(request, 'home.html', {'courses' : courses})
 
 def course_page(request, subject, number):
-    offerings = CourseInfo.objects.filter(subject=subject.upper(), course_number=int(number))
+    offerings: QuerySet[CourseInfo] = CourseInfo.objects.filter(subject=subject.upper(), course_number=int(number))
     unique_offerings = offerings.values('semester', 'year', 'instructor').distinct()
 
     avg_class_size = offerings.aggregate(Avg("students_enrolled"))["students_enrolled__avg"] or 0
-    return render(request, 'course_page.html', {'offerings' : unique_offerings, 'avg_class_size' : avg_class_size})
+
+    total_waitlist = offerings.aggregate(
+        total_waitlist=Sum(
+            (F('senior_requests') - F('seniors_enrolled')) +
+            (F('junior_requests') - F('juniors_enrolled')) +
+            (F('sophomore_requests') - F('sophomores_enrolled')) +
+            (F('first_year_requests') - F('first_years_enrolled'))))["total_waitlist"] or 0
+
+    zero = Value(0)
+
+    offerings = offerings.annotate(
+        waitlist_size =(
+        Greatest(F('senior_requests') - F('seniors_enrolled'), zero)+
+        Greatest(F('junior_requests') - F('juniors_enrolled'), zero)+
+        Greatest(F('sophomore_requests') - F('sophomores_enrolled'), zero)+
+        Greatest(F('first_year_requests') - F('first_years_enrolled'), zero)
+    ))
+
+    waitlist_count = offerings.filter(waitlist_size__gt=0).count()
+    total_offerings = offerings.count()
+    has_waitlist = waitlist_count >= total_offerings / 2 if total_offerings > 0 else False
+
+
+    return render(request, 'course_page.html', {
+        'offerings': unique_offerings,
+        'avg_class_size': avg_class_size,
+        'total_waitlist': total_waitlist,
+        'has_waitlist': has_waitlist,
+    })
+
+
+
+
 
 
